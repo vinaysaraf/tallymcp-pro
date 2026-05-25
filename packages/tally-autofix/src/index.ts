dc444a17f49826642b6b42f1426625120ce54543
Vinay Saraf <info@vinaysaraf.com>
2026-05-25 16:39:24 +0530

    feat(tally-autofix): public API exports + types

--- packages/tally-autofix/src/index.ts
+++ packages/tally-autofix/src/index.ts
@@ -1,2 +1,32 @@
-// Public exports populated by subsequent tasks.
-export {};
+export { TallyAutofixer, FIREWALL_RULE_NAME } from "./autofix.js";
+export type { TallyAutofixerOptions, FixXmlResult } from "./autofix.js";
+export {
+  detectTallyInstall,
+  type TallyInstall,
+  type DetectOptions,
+} from "./tally-detect.js";
+export {
+  detectTallyRunning,
+  waitForTallyClose,
+  waitForTallyHttp,
+  type WaitOptions,
+  type WaitForHttpOptions,
+} from "./tally-process.js";
+export {
+  RealExecRunner,
+  FakeExecRunner,
+  type ExecRunner,
+  type ExecResult,
+} from "./exec-runner.js";
+export {
+  parseTallyIni,
+  serializeTallyIni,
+  ensureXmlInterfaceLines,
+  TallyIni,
+} from "./tally-ini.js";
+export {
+  firewallRuleExists,
+  addFirewallRule,
+  removeFirewallRule,
+  type AddFirewallRuleOptions,
+} from "./firewall.js";
